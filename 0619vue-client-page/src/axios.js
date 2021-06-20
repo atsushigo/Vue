261b182196c3d866460c226b5ee2e4a3d675a3e7
sam921611 <sam921611@kimo.com>
2021-06-20 22:18:22 +0800

    把axios的headers封裝
    headers:{
    				Authorization:localStorage.getItem('token')
    			}
    變成
    axios.defaults.headers.common['Authorization'] = localStorage.getItem('token')

--- 0619vue-client-page/src/axios.js
+++ 0619vue-client-page/src/axios.js
@@ -1,3 +1,5 @@
 import axios from 'axios'
 
 axios.defaults.baseURL = 'http://localhost:????/'
+//Authorization 要特別注意後端有沒有帶 "Bearer "
+axios.defaults.headers.common['Authorization'] = localStorage.getItem('token')
--- 0619vue-client-page/src/components/Home.vue
+++ 0619vue-client-page/src/components/Home.vue
@@ -1,18 +1,23 @@
 <template>
-	<h3>Hello</h3>
+	<div>
+		<h3 v-if="user">Hello,{{ user }}</h3>
+		<h3 v-if="!user">You are not logged in</h3>
+	</div>
 </template>
 
 <script type="text/javascript">
 import axios from 'axios';
 export default {
 	name: 'Home',
+	data() {
+		return {
+			user: null
+		};
+	},
 	async created() {
-		const response = await axios.get('user',{
-			headers:{
-				Authorization:localStorage.getItem('token')
-			}
-		})
-		console.log(response)
+		this.user = response.data.user;
+		const response = await axios.get('user');
+		console.log(response);
 	}
 };
 </script>
