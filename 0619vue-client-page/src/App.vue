de850305c1099cc637f08b684aa4de7f9278d2cf
sam921611 <sam921611@kimo.com>
2021-06-20 18:38:59 +0800

    組件化 把APP的navbar抽到components做
    import Nav from'./components/Nav.vue';
    export default {
    	name: 'App'
    	components: {
    		Nav
    	}
    };

--- 0619vue-client-page/src/App.vue
+++ 0619vue-client-page/src/App.vue
@@ -1,24 +1,17 @@
 <template>
 	<div id="app">
-		<nav class="navbar navbar-expand navbar-light fixed-top">
-			<div class="container">
-				<a href="#" class="navbar-brand">Home</a>
-				<div class="collapse navbar-collapse">
-					<ul class="navbar-nav ml-auto">
-						<li class="nav-item"><a href="#" class="nav-link">Login</a></li>
-						<li class="nav-item"><a href="#" class="nav-link">Sign up</a></li>
-					</ul>
-				</div>
-			</div>
-		</nav>
-
+		<Nav />
 		<div class="auth-wrapper"><div class="auth-inner">Hello</div></div>
 	</div>
 </template>
 
 <script>
+import Nav from './components/Nav.vue';
 export default {
-	name: 'App'
+	name: 'App',
+	components: {
+		Nav
+	}
 };
 </script>
 
