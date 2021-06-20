64a847a9b6816f05b0e7a3166a68f24272b1307c
sam921611 <sam921611@kimo.com>
2021-06-20 19:33:38 +0800

    增加register頁面和register路由

--- 0619vue-client-page/src/router.js
+++ 0619vue-client-page/src/router.js
@@ -2,12 +2,15 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import Home from './components/Home.vue'
 import Login from './components/Login.vue'
+import Register from './components/Resgister.vue'
 
 Vue.use(Router)
 
 export default new Router({
+	mode:'history',
 	routes: [
 		{path: '/', component: Home},
-		{path: '/login', component: Login}
+		{path: '/login', component: Login},
+		{path: '/register', component: Register},
 		]
 })
