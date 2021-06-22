16bae7d9e26530963bf01ef0cbd19282a25ce2f0
sam921611 <sam921611@kimo.com>
2021-06-22 19:33:10 +0800

    增加 忘記密碼臨時url登入頁
    思路:忘記密碼頁面輸入電子郵件時，後端會寄驗證信(帶token的url)
    http://localhost:8080/reset/:token
    Ex: http://localhost:8080/reset/o838ogmhcLDKg2mnS01l-rgS
    
    到信箱，用戶點進去後進入修改密碼頁面，用戶修改完後把它導到Login頁面

---
+++ 0619vue-client-page/src/components/Reset.vue
@@ -0,0 +1,40 @@
+<template>
+	<form @submit.prevent>
+		<h3>Reset Password</h3>
+
+		<div class="form-group">
+			<label>Password</label>
+			<input v-model="password" type="password" class="form-control" placeholder="Password" />
+		</div>
+
+		<div class="form-group">
+			<label>Password Confirm</label>
+			<input v-model="passwordConfirm" type="password" class="form-control" placeholder="Password Confirm" />
+		</div>
+		
+		<button class="btn btn-primary btn-block" @click="handleSubmit()">Submit</button>
+	</form>
+</template>
+
+<script type="text/javascript">
+	import axios from 'axios'
+	export default {
+		name:'Reset',
+		data() {
+			return{
+				password:null,
+				passwordConfirm:null,
+			}
+		},
+		methods:{
+		async handleSubmit(){
+				const response = await axios.post('/reset',{
+					password:this.password,
+					token: this.$route.params.token
+				});
+				console.log(response)
+				this.$router.push('/login');
+			}
+		}
+	}
+</script>
--- 0619vue-client-page/src/router.js
+++ 0619vue-client-page/src/router.js
@@ -4,6 +4,7 @@ import Home from './components/Home.vue'
 import Login from './components/Login.vue'
 import Register from './components/Resgister.vue'
 import Forgot from './components/Forgot.vue'
+import Reset from './components/Reset.vue'
 
 Vue.use(Router)
 
@@ -14,5 +15,6 @@ export default new Router({
 		{path: '/login', component: Login},
 		{path: '/register', component: Register},
 		{path: '/forgot', component: Forgot},
+		{path: '/reset/:token', component: Reset},
 		]
 })
