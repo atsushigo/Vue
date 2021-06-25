f737a8f2bbdeefb4c5e0c22f6781eb1ce5ed051e
sam921611 <sam921611@kimo.com>
2021-06-25 19:16:55 +0800

    加上Navbar 直接放在App.vue的component

--- 0625-vue-blog-test/src/App.vue
+++ 0625-vue-blog-test/src/App.vue
@@ -1,25 +1,25 @@
 <template>
-  <div id="app">
-    
-	<!-- <AddBlog></AddBlog> -->
-	<!-- <ShowBlog></ShowBlog>-->
-	<router-view></router-view>
-  </div>
+	<div id="app">
+		<BlogHeader></BlogHeader>
+		<!-- <AddBlog></AddBlog> -->
+		<!-- <ShowBlog></ShowBlog>-->
+		<router-view></router-view>
+	</div>
 </template>
 
 <script>
 // import AddBlog from './components/AddBlog.vue'
 // import ShowBlog from './components/ShowBlog.vue'
+import BlogHeader from './components/BlogHeader.vue';
 
 export default {
-  name: 'App',
-  components: {
- //    AddBlog,
-	// ShowBlog
-  }
-}
+	name: 'App',
+	components: {
+		BlogHeader
+		//    AddBlog,
+		// ShowBlog
+	}
+};
 </script>
 
-<style>
-
-</style>
+<style></style>
---
+++ 0625-vue-blog-test/src/components/BlogHeader.vue
@@ -0,0 +1,45 @@
+<template>
+	<nav>
+		<ul>
+			<li>
+				<!-- exact是處理點下去後樣式殘留的問題 -->
+				<router-link to="/" exact>部落格</router-link>
+				<router-link to="/add" exact>撰寫部落格</router-link>
+			</li>
+		</ul>
+	</nav>
+</template>
+
+<script>
+export default {
+	name: 'BlogHeader'
+};
+</script>
+
+<style>
+ul {
+	list-style: none;
+	text-align: center;
+	margin: 0;
+}
+li {
+	display: inline-block;
+	margin: 0 10px;
+}
+a {
+	color: #fff;
+	text-decoration: none;
+	padding: 12px;
+	margin: 5px;
+	border-radius: 5px;
+}
+nav {
+	background: crimson;
+	padding: 30px 0;
+	margin-bottom: 40px;
+}
+.router-link-active {
+	background: rgba(255, 255, 255, 0.8);
+	color: #444;
+}
+</style>
