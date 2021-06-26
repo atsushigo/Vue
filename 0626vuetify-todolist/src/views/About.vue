60225f698e040839ff06154ec76aa76dc43d0a07
sam921611 <sam921611@kimo.com>
2021-06-27 02:39:24 +0800

    update

--- 0626vuetify-todolist/src/views/About.vue
+++
@@ -1,6 +0,0 @@
-<template>
-  <div class="about pa-6">
-    <h1>This is an about page</h1>
-	<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsum sed quae beatae velit explicabo consequatur consectetur exercitationem quos! Nulla unde qui maxime officiis consectetur neque deserunt totam soluta maiores esse.</p>
-  </div>
-</template>
