818420e5a2ed1cb0b36202d64615405d42228f88
sam921611 <sam921611@kimo.com>
2021-06-27 00:59:13 +0800

    建外層UI

--- 0626vuetify-todolist/src/App.vue
+++ 0626vuetify-todolist/src/App.vue
@@ -1,55 +1,59 @@
 <template>
-  <v-app>
-    <v-app-bar
-      app
-      color="primary"
-      dark
-    >
-      <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
+  <v-app id="inspire">
+    <v-navigation-drawer permanent v-model="drawer" app>
+          <v-list-item>
+            <v-list-item-content>
+              <v-list-item-title class="text-h6">
+                Vuetify To
+              </v-list-item-title>
+              <v-list-item-subtitle>
+                Todo項目測試
+              </v-list-item-subtitle>
+            </v-list-item-content>
+          </v-list-item>
+    
+          <v-divider></v-divider>
+    
+          <v-list
+            dense
+            nav
+          >
+            <v-list-item
+              v-for="item in items"
+              :key="item.title"
+              link
+            >
+              <v-list-item-icon>
+                <v-icon>{{ item.icon }}</v-icon>
+              </v-list-item-icon>
+    
+              <v-list-item-content>
+                <v-list-item-title>{{ item.title }}</v-list-item-title>
+              </v-list-item-content>
+            </v-list-item>
+          </v-list>
+        </v-navigation-drawer>
 
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
-      </div>
+    <v-app-bar app>
+      <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
 
-      <v-spacer></v-spacer>
-
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
-      </v-btn>
+      <v-toolbar-title>Application</v-toolbar-title>
     </v-app-bar>
 
     <v-main>
-      <router-view/>
+      <!--  -->
     </v-main>
   </v-app>
 </template>
 
 <script>
-
-export default {
-  name: 'App',
-
-  data: () => ({
-    //
-  }),
-};
+  export default {
+    data: () => ({ 
+		drawer: null ,
+		items: [
+		          { title: 'Todo', icon: 'mdi-view-dashboard' },
+		          { title: 'About', icon: 'mdi-help-box' },
+		        ],
+	}),
+  }
 </script>
