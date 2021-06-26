8b3f483accbc4ab5ab8065381eb61409d1121e09
sam921611 <sam404101270@gmail.com>
2021-06-26 14:31:33 +0800

    Delete 0625-vue-blog-test directory

--- 0625-vue-blog-test/src/myaxios.js
+++
@@ -1,8 +0,0 @@
-import axios from 'axios'
-
-const myaxios = axios.create({
-	baseURL:"https://jsonplaceholder.typicode.com",
-	// headers:common['Authorization'] = localStorage.getItem('token')
-})
-
-export default myaxios
