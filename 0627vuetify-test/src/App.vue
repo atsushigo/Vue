812f16476e032dcc2d6d4cfa4472965d844a2cc7
sam921611 <sam921611@kimo.com>
2021-06-27 16:10:12 +0800

    增加navagation drawer內容 和main區 router-view

--- 0627vuetify-test/src/App.vue
+++ 0627vuetify-test/src/App.vue
@@ -1,26 +1,34 @@
 <template>
-  <v-app id="inspire">
-    <v-navigation-drawer
-      v-model="drawer"
-      app
-    >
-      <!--  -->
-    </v-navigation-drawer>
+	<v-app id="inspire">
+		<v-navigation-drawer v-model="drawer" app>
+			<v-list dense>
+				<v-list-item link>
+					<v-list-item-action><v-icon>mdi-home</v-icon></v-list-item-action>
+					<v-list-item-content><v-list-item-title>Home</v-list-item-title></v-list-item-content>
+				</v-list-item>
+				<v-list-item link>
+					<v-list-item-action><v-icon>mdi-email</v-icon></v-list-item-action>
+					<v-list-item-content><v-list-item-title>Contact</v-list-item-title></v-list-item-content>
+				</v-list-item>
+			</v-list>
+		</v-navigation-drawer>
 
-    <v-app-bar app>
-      <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
+		<v-app-bar app>
+			<v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
 
-      <v-toolbar-title>Application</v-toolbar-title>
-    </v-app-bar>
+			<v-toolbar-title>Application</v-toolbar-title>
+		</v-app-bar>
 
-    <v-main>
-      <!--  -->
-    </v-main>
-  </v-app>
+		<v-main>
+			<!--  -->
+			<!-- <h1>main message here</h1>-->
+			<router-view></router-view>
+		</v-main>
+	</v-app>
 </template>
 
 <script>
-  export default {
-    data: () => ({ drawer: null }),
-  }
+export default {
+	data: () => ({ drawer: true })
+};
 </script>
