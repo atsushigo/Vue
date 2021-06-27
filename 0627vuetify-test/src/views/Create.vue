e1bd6709dd5a9ce051e7b5071b6dd08fc2d3a403
sam921611 <sam921611@kimo.com>
2021-06-28 01:43:17 +0800

    Create組件拆分共用Form表單(ProductForm)
    Create-->獨立組件(增刪修的增)
    ProductForm-->共用組件(增刪修共用表單組件)
    router沒動過
    
    <ProductForm :product="product" :onSubmit="submit"></ProductForm>
    
    真正的submit方法是在獨立組件呼叫，運算完後把值傳給共用組件(Form)。
    
    用props傳data裡面的product和onSubmit值(方法)

--- 0627vuetify-test/src/views/Create.vue
+++ 0627vuetify-test/src/views/Create.vue
@@ -1,65 +1,50 @@
 <template>
-	<!-- <h1>Create</h1>-->
-	<v-form class="ma-5" ref="form" v-model="valid" lazy-validation>
-		<v-text-field v-model="product.title" :counter="10" :rules="titleRules" label="Title" required></v-text-field>
-		<!-- <v-text-field multi-line v-model="product.descriotion" :rules="emailRules" label="Description" required></v-text-field> -->
-		<v-textarea
-			name="input-7-1"
-			v-model="product.description"
-			filled
-			label="Label"
-			auto-grow
-			value="The Woodman set to work at once, and so sharp was his axe that the tree was soon chopped nearly through."
-		></v-textarea>
-		<v-text-field :rules="priceRules" prefix="$" v-model.number="product.price" label="Price" required></v-text-field>
-		<v-text-field :rules="quantityRules" v-model.number="product.quantity" label="Quantity" required></v-text-field>
-		<v-text-field :rules="imageRules" v-model.trim="product.image" label="Image" required></v-text-field>
-		<v-btn :disabled="!valid" color="success" class="mr-4" @click="submit()">Submit</v-btn>
-
-		<v-btn color="error" class="mr-4" @click="clear()">Clear</v-btn>
-	</v-form>
+	<!-- 這邊要注意onSubmit傳過來是用值的狀態要用submit接不可以submit() TypeError -->
+	<!-- 可以理解為個組件中間只傳值 而不是function -->
+	<ProductForm :product="product" :onSubmit="submit"></ProductForm>
 </template>
 
-<script type="text/javascript">
+<script>
 import API from '@/lib/API';
+import ProductForm from './ProductForm.vue';
 export default {
 	name: 'Create',
+	components: {
+		ProductForm
+	},
 	data() {
 		return {
-			valid: true,
 			product: {
 				title: '',
 				description: '',
 				price: 0,
 				quantity: 0,
 				image: ''
-			},
-			titleRules: [
-				title => {
-					if (title.trim() === '') return 'Title must no be empty';
-				}
-			],
-			priceRules: [
-				price => {
-					if (isNaN(price)) return 'Price must be a number';
-					if (Number(price) <= 0) return 'Price must be greater than 0';
-				}
-			],
-			quantityRules: [
-				quantity => {
-					if (isNaN(quantity)) return 'Quantity must be a valid number';
-					if (Number(quantity) < 0) return 'Quantity must be 0 or greater';
-				}
-			],
-			imageRules: [
-				image => {
-					if (!image) return 'Please enter image';
-				}
-			]
+			}
 		};
 	},
 	methods: {
 		submit() {
+			// 特別注意轉型
+			// this.product.quantity = Number(this.product.quantity);
+			API.createProduct(this.product)
+				.then(res => {
+					console.log(res);
+					//接回傳的對象拿id跳轉頁面
+					this.$router.push("'/product/'+res.id");
+				})
+				.catch(err => {
+					console.log(err);
+					this.$router.push("'/product/'+res.id");
+				});
+		}
+	}
+};
+</script>
+
+<style></style>
+
+<!-- submit() {
 			// console.log(this.product)
 			if (this.$refs.form.validate()) {
 				// 特別注意轉型
@@ -73,15 +58,8 @@ export default {
 					.catch(err => {
 						console.log(err);
 					});
+				
 			} else {
 				alert('not OK');
 			}
-		},
-		clear() {
-			this.$refs.form.reset();
-		}
-	}
-};
-</script>
-
-<style type="text/css"></style>
+		}, -->
