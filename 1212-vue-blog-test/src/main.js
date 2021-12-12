629a7842f86c12db15588b6d1e1e3fe2ba8255e6
sam921611 <sam921611@kimo.com>
2021-12-12 14:14:19 +0800

    導入element-ui 和 axios
    npm i element-ui --save
    npm i axios
    
    main.js 全局設置
    全局設置axios
    import axios from 'axios'
    Vue.prototype.$axios = axios
    
    全局設置element樣式
    import Element from 'element-ui'
    import 'element-ui/lib/theme-chalk/index.css'
    
    Vue.use(Element)

--- 1212-vue-blog-test/src/main.js
+++ 1212-vue-blog-test/src/main.js
@@ -2,8 +2,15 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import Element from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+import axios from 'axios'
 
 Vue.config.productionTip = false
+//全局設置axios
+Vue.prototype.$axios = axios
+
+Vue.use(Element)
 
 new Vue({
   router,
