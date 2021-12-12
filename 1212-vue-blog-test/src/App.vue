c733d69bceb5c368e3008f135b25a812138169c4
sam921611 <sam921611@kimo.com>
2021-12-12 16:11:14 +0800

    處理佈局和LOGIN頁面佈局+校驗規則

--- 1212-vue-blog-test/src/App.vue
+++ 1212-vue-blog-test/src/App.vue
@@ -1,9 +1,50 @@
 <template>
-  <div id="app">
-    <router-view/>
-  </div>
+	<div id="app">
+		<el-container>
+			<el-header><img class="logo" src="https://upload.wikimedia.org/wikipedia/commons/thumb/3/3e/Blogger_logo.svg/1280px-Blogger_logo.svg.png" /></el-header>
+			<el-main>Main</el-main>
+		</el-container>
+		<router-view />
+	</div>
 </template>
 
 <style>
+.logo {
+	height: 60%;
+	margin-top: 0.4125rem;
+}
+.el-header,
+.el-footer {
+	background-color: #b3c0d1;
+	color: #333;
+	text-align: center;
+	line-height: 60px;
+}
 
+.el-aside {
+	/* background-color: #d3dce6; */
+	color: #333;
+	text-align: center;
+	line-height: 200px;
+}
+
+.el-main {
+	/* background-color: #e9eef3; */
+	color: #333;
+	text-align: center;
+	line-height: 160px;
+}
+
+body > .el-container {
+	margin-bottom: 40px;
+}
+
+.el-container:nth-child(5) .el-aside,
+.el-container:nth-child(6) .el-aside {
+	line-height: 260px;
+}
+
+.el-container:nth-child(7) .el-aside {
+	line-height: 320px;
+}
 </style>
--- 1212-vue-blog-test/src/views/Login.vue
+++ 1212-vue-blog-test/src/views/Login.vue
@@ -1,15 +1,72 @@
 <template>
-	<div>
-		Login
-	</div>
+	<el-form :model="ruleForm" :rules="rules" ref="ruleForm" label-width="100px" class="demo-ruleForm">
+	  <el-form-item label="用戶名" prop="username">
+	    <el-input v-model="ruleForm.username"></el-input>
+	  </el-form-item>
+	  <el-form-item label="密碼" prop="password">
+	    <el-input  type="password" v-model="ruleForm.password"></el-input>
+	  </el-form-item>
+	  
+	  <el-form-item>
+	    <el-button type="primary" @click="submitForm('ruleForm')">立即创建</el-button>
+	    <el-button @click="resetForm('ruleForm')">重置</el-button>
+	  </el-form-item>
+	</el-form>
 </template>
 
 <script>
 	export default {
-		
-	}
+	    data() {
+	      return {
+	        ruleForm: {
+	          name: '',
+	          region: '',
+	          date1: '',
+	          date2: '',
+	          delivery: false,
+	          type: [],
+	          resource: '',
+	          desc: ''
+	        },
+	        rules: {
+	          username: [
+	            { required: true, message: '請輸入用戶名稱', trigger: 'blur' },
+	            { min: 3, max: 5, message: '长度在 3 到 5 个字符', trigger: 'blur' }
+	          ],
+	          password: [
+	            { required: true, message: '請輸入密碼', trigger: 'blur' },
+	            /* { min: 3, max: 5, message: '长度在 3 到 5 个字符', trigger: 'blur' } */
+	          ],
+	          
+	        }
+	      };
+	    },
+	    methods: {
+	      submitForm(formName) {
+	        this.$refs[formName].validate((valid) => {
+	          if (valid) {
+	            alert('submit!');
+				this.$axios.post("http://localhost:8081/login",this.ruleForm).then(res=>{
+					console.log("請求成功",res)
+					const jwt = res.headers['authorization']
+					
+				})
+	          } else {
+	            console.log('error submit!!');
+	            return false;
+	          }
+	        });
+	      },
+	      resetForm(formName) {
+	        this.$refs[formName].resetFields();
+	      }
+	    }
+	  }
 </script>
 
 <style scoped>
-
+.demo-ruleForm{
+	max-width: 500px;
+	margin: 0 auto;
+}
 </style>
