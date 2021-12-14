dc4ee17f367d046ff026eb19ea56ab993c329c13
sam921611 <sam921611@kimo.com>
2021-12-14 23:15:51 +0800

    BlogEdit元件:增加mardown編輯器
    npm i mavon-editor --save
    main.js中引入並使用
    安裝markdown編輯器
    
    表單包括
    標題
    摘要
    內容
    
    內容部分用markdown編輯
    <mavon-editor v-model="ruleForm.content"></mavon-editor>

--- 1212-vue-blog-test/src/main.js
+++ 1212-vue-blog-test/src/main.js
@@ -6,12 +6,16 @@ import Element from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
 import axios from 'axios'
 import "./axios"
+import mavonEditor from "mavon-editor"
+
+import "mavon-editor/dist/css/index.css"
 
 Vue.config.productionTip = false
 //全局設置axios
 Vue.prototype.$axios = axios
 
 Vue.use(Element)
+Vue.use(mavonEditor)
 
 new Vue({
   router,
