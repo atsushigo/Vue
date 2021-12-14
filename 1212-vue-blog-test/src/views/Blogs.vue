dfff5745212ee03e14e85879060c133f5e7b2bc2
sam921611 <sam921611@kimo.com>
2021-12-14 22:58:27 +0800

    引用後端取到的值for循環渲染

--- 1212-vue-blog-test/src/views/Blogs.vue
+++ 1212-vue-blog-test/src/views/Blogs.vue
@@ -4,19 +4,35 @@
 
     <div class="block">
       <el-timeline>
-        <el-timeline-item timestamp="2018/04/12" placement="top">
+        <!-- 時間需要再格式化 toLocaleString() -->
+        <el-timeline-item
+          :timestamp="blog.created"
+          placement="top"
+          v-for="(blog, index) in blogs"
+          :key="index"
+        >
           <el-card>
-            <h4>更新模板</h4>
-            <p>提交於2021/04/12 20:46</p>
+            <h4>
+              <router-link
+                :to="{ name: 'BlogDetail', params: { blogId: blog.id } }"
+              >
+                {{ blog.title }}
+              </router-link>
+            </h4>
+            <p>{{ blog.description }}</p>
           </el-card>
         </el-timeline-item>
       </el-timeline>
 
+      <!-- element組件寫法 @current-change    https://element.eleme.io/#/zh-CN/component/pagination#attributes-->
       <el-pagination
         class="mpage"
         background
         layout="prev, pager, next"
-        :total="1000"
+        :current-page="currentPage"
+        :page-size="pageSize"
+        :total="total"
+        @current-change="page(this.currentPage)"
       ></el-pagination>
     </div>
   </div>
@@ -31,6 +47,7 @@ export default {
       //默認取第一頁
       blogs: {},
       currentPage: 1,
+      total: 1,
       pageSize: 5,
     };
   },
@@ -42,7 +59,7 @@ export default {
         console.log(res);
 
         //後端返回的名稱records current
-		//前端變數得到數據渲染
+        //前端變數得到數據渲染
         _this.blogs = res.data.data.records;
         _this.currentPage = res.data.data.currentPage;
         _this.total = res.data.data.total;
@@ -50,10 +67,10 @@ export default {
       });
     },
   },
-  created(){
-	  //呼叫page方法 默認第一頁渲染
-	  this.page(1)
-  }
+  created() {
+    //呼叫page方法 默認第一頁渲染
+    this.page(1);
+  },
 };
 </script>
 
